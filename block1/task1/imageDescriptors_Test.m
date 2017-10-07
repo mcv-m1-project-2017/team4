a5dbf49b818ac4465a09e79a3c4963de2dd83ca6
marticobos <32466018+marticobos@users.noreply.github.com>
2017-10-07 16:16:01 +0200

    Script to obtain signal characteristics histogram

--- block1/task1/imageDescriptors_Test.m
+++ block1/task1/imageDescriptors_Test.m
@@ -1,8 +1,58 @@
 
+clear all
+%%function to evaluate all images and obtain all characteristics
+imagePath = '..\..\..\train\';
+nDescriptors = 18;
 
-BB = [113.190000 1291.880000 175.660000 1361.820000];
-% img = imread('C:\Users\Marti\Desktop\MCV\M1-IHCV\Project\train\00.000977.jpg');
-% mask = imread('C:\Users\Marti\Desktop\MCV\M1-IHCV\Project\train\mask\mask.00.000977.png');
-img = imread('..\train\00.000977.jpg');
-mask = imread('..\train\mask\mask.00.000977.png');
-descriptors = imageDescriptors (img, mask, BB);
+files = dir(strcat(imagePath,'/*.jpg'));
+nSigns = size(files,1);
+signalGroup = cell(1,1);
+imageCharacteristics = zeros(1,nDescriptors);
+
+for i=1:nSigns
+    i
+    imageName = files(i).name(1:end-4)
+    img = imread(strcat(imagePath, imageName,'.jpg'));
+    mask = imread(strcat(imagePath,'mask\mask.', imageName,'.png'));
+    fid = fopen(strcat(imagePath,'gt\gt.', imageName,'.txt'));
+    while ~(feof(fid))
+        imgData = fgetl(fid);
+        imgData = strsplit(imgData);
+        BB = [str2double(imgData{1}) str2double(imgData{2}) str2double(imgData{3}) str2double(imgData{4})];
+        signalGroup{i} = imgData{5};
+        descriptors = imageDescriptors (img, mask, BB);
+        imageCharacteristics(i,:)= descriptors;
+    end
+    fclose(fid);
+end
+
+[nImages nChars] = size(imageCharacteristics)
+group = zeros(nImages,1);
+for i = 1:nImages
+    if strcmp(signalGroup{i},'A')==1
+        group(i) = 1;
+    elseif strcmp(signalGroup{i},'B')==1
+        group(i) = 2;
+    elseif strcmp(signalGroup{i},'C')==1
+        group(i) = 3;
+    elseif strcmp(signalGroup{i},'D')==1
+        group(i) = 4;
+    elseif strcmp(signalGroup{i},'E')==1
+        group(i) = 5;
+    elseif strcmp(signalGroup{i},'F')==1
+        group(i) = 6;
+    end
+
+end
+
+for i=1:nChars
+    figure()
+    data = imageCharacteristics(:,i);
+    histogram(data(group ==1));
+    hold on;
+    histogram(data(group ==2));
+    histogram(data(group ==3));
+    histogram(data(group ==4));
+    histogram(data(group ==5));
+    histogram(data(group ==6));
+end
