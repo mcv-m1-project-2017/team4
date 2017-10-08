462a24287c51196458f264e260b45fc9990155d3
Jonatan <jonatan@adsmurai.com>
2017-10-08 16:59:44 +0200

    Save mask to a file

--- block1/task3/predict.m
+++ block1/task3/predict.m
@@ -2,8 +2,16 @@ function [ paths_of_computed_masks ] = predict(features, paths)
   % COLOUR SEGMENTATION PREDICTION: Given an average colour histograms do a 
   %   segmentation and save it as a mask in a PNG format.
   global number_of_classes
+  show_masks = false;
+  output_folder = fullfile(fileparts(fileparts(paths(1,:))), 'validation', 'mask');
+  
   for i=1:size(paths)
-    image = imread(paths(i,:));
+    image_path = paths(i,:);
+    image_filename = strsplit(image_path, '/'){end};
+    image_name = strcat(strsplit(image_filename, '.'){1},
+                        '.',
+                        strsplit(image_filename, '.'){2});
+    image = imread(image_path);
     
     % If all the three colour channels are equal than feature of a class, the 
     %  pixel belongs to that class.
@@ -17,7 +25,7 @@ function [ paths_of_computed_masks ] = predict(features, paths)
     for class=1:number_of_classes
       % If all three colour channels are similar than feature of a class, mark 
       %  that pixel belonging to that class.
-      threshold = 10;
+      threshold = 25;
       red_mask = (image(:,:,1) > features(class).r - threshold) & ...
                  (image(:,:,1) < features(class).r + threshold);
       green_mask = (image(:,:,2) > features(class).g - threshold) & ...
@@ -26,15 +34,17 @@ function [ paths_of_computed_masks ] = predict(features, paths)
                   (image(:,:,3) < features(class).b + threshold);
       mask |= red_mask & green_mask & blue_mask;
 
-      figure(2)
-      imshow(image)
-      figure(3)
-      imshow(mask, [0, 1])
-      pause(1)
+%      figure(2), imshow(image)
+%      figure(3), imshow(mask, [0, 1])
+%      pause(1)
     end
-    imshow(mask, [0, 1])
-    pause(3)
-      
+    if show_masks
+      imshow(mask, [0, 1])
+      pause(3)
+    end
+    output_file = fullfile(output_folder, strcat('mask.', image_name, '.png'));
+    fprintf('Saving mask %s to file %s\n', image_name, output_file);
+    imwrite(mask, output_file, 'PNG');
   end
 
   paths_of_computed_masks = [
