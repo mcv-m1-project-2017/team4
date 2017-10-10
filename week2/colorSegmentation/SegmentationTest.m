4f57e938f9cb080391f40a06fe8d9cc46b7ee524
marticobos <32466018+marticobos@users.noreply.github.com>
2017-10-10 21:30:58 +0200

    code adapted to traffic signs datasets

--- week2/colorSegmentation/SegmentationTest.m
+++ week2/colorSegmentation/SegmentationTest.m
@@ -1,74 +1,31 @@
 
-    [numTest, txtTest, rawTest] = xlsread('F:\Image BBDD\BBDD.xlsm');
-    path = 'F:\Image BBDD\';
+    
+    path = '..\Datasets\train';
+    files=dir(strcat(path,'/*.jpg'));
     time_t6 = [];
     time_t4 = [];
-    for i=2:10%length(txtTest(:,1))
-        if(isempty(char(txtTest(i,20)))==0)
-                obj.i=i-1
-                obj.filepath=strcat(path,'Images',char(txtTest(i,21)),char(txtTest(i,20)));
-                
-                im = double(imread(strcat(obj.filepath,'_RD.bmp')));
-                
+    for i=1:length(files)
+                i
+                filepath=fullfile(path, files(i).name);
+                close all;
+                im = imread(filepath);
+                imshow(im);
+                figure;
                 tic;
-                maskR = ColorSegmentation(im,'red');
-                maskB = ColorSegmentation(im,'blue');
+                maskR = ColorSegmentationt4(im,'red');
+                maskB = ColorSegmentationt4(im,'blue');
                 mask = maskR|maskB;
                 t=toc;
                 time_t6(length(time_t6)+1) = t;
                 tic;
-                mask1 = ColorSegmentationMod(im);
+                mask1 = colorSegmentation(im);
                 t=toc;
+                imshow(mask1,[]);
                 time_t4(length(time_t4)+1) = t;
-                
-                im = double(imread(strcat(obj.filepath,'_GR.bmp')));
-                
-                tic;
-                maskR = ColorSegmentation(im,'red');
-                maskB = ColorSegmentation(im,'blue');
-                mask = maskR|maskB;
-                t=toc;
-                time_t6(length(time_t6)+1) = t;
-                tic;
-                mask = ColorSegmentationMod(im);
-                t=toc;
-                time_t4(length(time_t4)+1) = t;
-                
-                
-                im = double(imread(strcat(obj.filepath,'_BL.bmp')));
-                
-                tic;
-                maskR = ColorSegmentation(im,'red');
-                maskB = ColorSegmentation(im,'blue');
-                mask = maskR|maskB;
-                t=toc;
-                time_t6(length(time_t6)+1) = t;
-                tic;
-                mask = ColorSegmentationMod(im);
-                t=toc;
-                time_t4(length(time_t4)+1) = t;
-                
-                im = double(imread(strcat(obj.filepath,'.bmp')));
-                
-                
-                tic;
-                maskR = ColorSegmentation(im,'red');
-                maskB = ColorSegmentation(im,'blue');
-                mask = maskR|maskB;
-                t=toc;
-                time_t6(length(time_t6)+1) = t;
-                tic;
-                mask = ColorSegmentationMod(im);
-                t=toc;
-                time_t4(length(time_t4)+1) = t;
-                
-        end
+
     end
     
 %     frpintf('-------------------------------------------------------\n');
 %     frpintf('Time per frame team 6 segmentation:');
     mean(time_t6)
     
-%     frpintf('-------------------------------------------------------\n');
-%     frpintf('Time per frame team 4 segmentation:');
-    mean(time_t4)
