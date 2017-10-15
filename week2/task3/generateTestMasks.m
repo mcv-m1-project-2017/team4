270e3aae19b70d51c4a418dfe7dc4ca605b02871
marticobos <32466018+marticobos@users.noreply.github.com>
2017-10-15 12:21:06 +0200

    bugfix

--- week2/task3/generateTestMasks.m
+++ week2/task3/generateTestMasks.m
@@ -57,18 +57,3 @@ for i = 1:size(files)
 end
 
 
-%Compute algorithm precision, accuracy, specificity, recall and fmeasure
-[pixelPrecision, pixelAccuracy, pixelSpecificity, pixelRecall] = PerformanceEvaluationPixel(pixelTP, pixelFP, pixelFN, pixelTN);
-FMeasure = 2*(pixelPrecision*pixelRecall)/(pixelPrecision+pixelRecall);
-total = pixelTP + pixelFP + pixelFN + pixelTN;
-pixelTP = pixelTP / total;
-pixelFP = pixelFP / total;
-pixelFN = pixelFN / total;
-
-%Get time per frame mean
-timePerFrame = mean(processingTimes);
-
-%Print results in array format
-fprintf('----------------------------------------------------\n');
-fprintf('Algorithm results: pixelPrecision; pixelAccuracy; pixelRecall; Fmeasure; pixelTP; pixelFP; pixelFN; timePerFrame');
-[pixelPrecision pixelAccuracy pixelRecall FMeasure pixelTP pixelFP pixelFN timePerFrame]
