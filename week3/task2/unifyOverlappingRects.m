8cea263f60f4b3f93a59bfda9758c60beec0eaff
cesc <francesc.serracant@gmail.com>
2017-10-26 08:58:24 +0200

    Comments on unifyOverlappingRects

--- week3/task2/unifyOverlappingRects.m
+++ week3/task2/unifyOverlappingRects.m
@@ -4,20 +4,27 @@ function l = unifyOverlappingRects(rects)
   %         rectangles in rects are merged into its common bounding box.
   %         Not that this is not recursive, so there may be overlapping
   %         rectangles on the output.
+  
   result = [];
   
   [rows cols] = size(rects);
+  % while there are still rects ...
   while (rows > 0)
+    % get rects that overlap with the first one on the list
     group = rects(rectint(rects, rects(1,:))>0,:);
     
+    % remove those rects from the list
     rects = setdiff(rects, group,'rows');
     
+    % get the outer bounding box for the overlapping rects
     x1 = min( group(:,1) );
     y1 = min( group(:,2) );
     x2 = max( group(:,1) + group(:,3) );
     y2 = max( group(:,2) + group(:,4) );
     
+    % add outer bounding box to the result
     result = [result; x1 y1 x2-x1 y2-y1];
+    
     [rows cols] = size(rects);
   end
   
