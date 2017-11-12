d08be0bbd62e35aea751250c402ac204a4b56ad5
cesc <francesc.serracant@gmail.com>
2017-11-12 11:21:42 +0100

    improvements

---
+++ week5/task2/angleLines.m
@@ -0,0 +1,5 @@
+function angle = angleLines(points1, points2, k1, k2)       
+    v1 = points2(k1,:)-points1(k1,:);
+    v2 = points2(k2,:)-points1(k2,:);
+    angle = abs(rad2deg( atan2( det([v1;v2]), dot(v1,v2) ) ));
+end
